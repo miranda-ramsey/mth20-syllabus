979fe4a266cd05c10aab0e6b8a7978ccd8691eed
bunnysnack <miranda.j.ramsey@gmail.com>
2025-03-05 02:21:31 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Miranda Ramsey Math Department Portland Community College      copyright         "
+},
+{
+  "id": "basic-needs",
+  "level": "1",
+  "url": "basic-needs.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Basic Needs Statement",
+  "body": " Basic Needs Statement  PCC wants you to be successful and have the resources that you need. If you need help with food, housing, healthcare, or other resources, check out these resources . You can also contact conductandcare@pcc.edu to connect with a person at PCC who will help you find support.    "
+},
+{
+  "id": "course-description",
+  "level": "1",
+  "url": "course-description.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Description",
+  "body": " Course Description  Taken from the official PCC course description:   Course Number  MTH 20    Course Title  Fundamentals of Mathematics    Credit Hours  4    Lecture Hours  30    Lecture\/Lab Hours  20    Prerequisites   ABE 0782 or placement into MTH 20, and  placement into RD 80 or ESOL 250.      Develops arithmetic fluency and the conceptual basis, and applications of integers, fractions, decimals, percents, and measurements. The PCC math department recommends that students take MTH courses in consecutive terms. Audit available.   Learning Outcomes  Upon completion of the course students should be able to:   Perform accurate arithmetic computations in a variety of expressions and applications.    Apply mathematical problem solving strategies.    Effectively communicate mathematical reasoning.      The complete Course Content and Outcome Guide can be found at www.pcc.edu\/ccog\/ .  "
+},
+{
+  "id": "instructor",
+  "level": "1",
+  "url": "instructor.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Instructor Information",
+  "body": " Instructor Information   Contact  To reach me or PCC:     Instructor  Miranda Ramsey    Office  SE SCOM 214    Phone  971-722-3542 Voicemail only. Not a good way to reach me.    Email  miranda.ramsey@pcc.edu    Address   Portland Community College  Southeast Campus  12000 SW 49th Ave.  Portland, OR 97219        Communication  I highly encourage you to exchange information with your peers, especially those you are working with in groups, to work together outside of class. Email, group chats, and Discord servers are all excellent ways to connect and collaborate.  The best way to contact me is through email. I check my email often! please email me at miranda.ramsey@pcc.edu . Due to federal privacy laws, We must use your PCC email address to communicate, so make sure you are contacting me from your PCC email address or the D2L Classlist feature, and checking your PCC email regularly for my replies.  Emails sent on weekdays will be answered within 24 hours. Emails sent over a weekend or holiday will be answered the following scheduled school day.    Office Hours  I have scheduled office hours where you can come see me in person, or we can schedule a Zoom appointment as needed. I encourage you to come to office hours whenever you need additional help.       Day  Location  Time     Mondays    Student Learning Center    12:00 1:00      Wednesdays    Student Learning Center    12:00 1:00      Fridays    Zoom    By Appointment       Instructional Approach   Mathematics has an unfortunate reputation for being a challenging subject that only some people are capable of engaging with. I strongly believe that this is not true, and that more people are capable of mathematics than we give credit to. My goal is to facilitate creativity and discovery, rather than simply dictating facts and procedures to memorize. In my lectures I set out to establish a question that can ground us and give us context for the math we are working with, and through group work students will be encouraged to talk through problems, ask for help, and lift each other up.    Weekly Structure  To perform best in the class, each week you should:   Read Ahead  Read the textbook material ahead of the days we will be covering it in class, so that you can ask questions and get clarification during lectures and group work.    Do Homework As We Go  Work on the homework throughout the week, rather than waiting until the end of the week. This way, challenging problems can be brought up in class with me or with your group.    Come to Office Hours  Visit me in Office Hours to fill in gaps in your understanding, before they become stressful issues.        My Schedule           Class  CRN  Days  Time  Place  Midterm  Final    MTH 111  20518  MW  9:00 11:50  LIBR 216  Wed 4\/30  Mon 6\/9    MTH 20  25135  MW  2:00 4:20  SCOM 320  Wed 4\/30  Wed 6\/11    Exams are in class starting at the usual class time. For details, see .   "
+},
+{
+  "id": "materials",
+  "level": "1",
+  "url": "materials.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Class Materials",
+  "body": " Class Materials   Textbook  For this class we will be using Basic College Mathematics (Third Edition) . Most of the structure of the class will be built around chapters 1-6, with chapters 7-11 used as references for applications throughout the term.  Purchase this book at the PCC book store.     Supplies    Pencils and Erasers. Mistakes happen in math, and trying to correct mistakes made with an ink pen is challenging and creates messy work.    A 1-inch, 3-ring binder for organizing your Workbook activities and notes, unless using a digital alternative.     Graph Paper for notes and assignments. My favorite choice is Engineering Paper , which is constructed to have faint lines on the front for a clean appearance, but any graph paper will do.      Technology    Regular access to D2L (Desire2Learn) will be required to access materials and submit assignments for this class. Please visit the technology support section of Student Support Resources for a complete list of apps and tools for PCC students.    A personal scientific calculator such as a TI-30XS may be helpful for this class, but is not required.     "
+},
+{
+  "id": "attendance",
+  "level": "1",
+  "url": "attendance.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Attendance",
+  "body": " Attendance   Attendance Policy  Attendance and participation are crucial to succeed in a math course. In this class, Group Work will be a large component, so students are expected to attend class and collaborate with each other. Credit for group work will not be given if students are absent or not participating.  However, I understand that perfect attendance is not a practical expectation. If you must miss class, please email me to let me know, and I will work with you to make sure you keep up and that your grade is minimally affected.    First Week Activities  During the first week of the term, instructors must identify students who are enrolled but not engaged in a class. These are referred to as \"No Show\" students and will be dropped from the class as per PCC's G302 Grading Guidelines Policy.   In order to avoid being dropped as a \"No Show\" student, you must complete the following activities by their deadlines:     Attend and participate in class during the first week (Monday, March 31 and Wednesday, April 2)    Complete the MTH 20 Student Introduction form before class Wednesday, April 2.      "
+},
+{
+  "id": "grades",
+  "level": "1",
+  "url": "grades.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Grades",
+  "body": " Grades   For general information about grades, please go to the PCC Grading Guidelines.   Grades    Assignment  Number  Weight    Homework Sets  7  20%    Workbook Activities  18  20%    Quizzes  7  10%    Midterm Exam  1  20%    Final Exam  1  30%       Assignments     Homework Sets  Each week you will have a set of homework problems to complete and submit on your own.    Workbook Activities  For each class session there is an associated workbook activity. For most activities, there is a prep-work portion, a group-work portion, and a post-work reflection portion. It is your responsibility to arrive to class with the prep work completed so that you can fully participate with your group. The workbook will be turned in with a self assessment before each Exam.    Quizzes  There will be seven quizzes, each administered on a Wednesday at the start of class and covering material from the week before.   Quiz Corrections: After your graded quiz is returned, you may redo the quiz in its entirety and submit it as a Quiz Correction. Your final grade will be an average of your two submissions, meaning you can recover up to half of your missed points by submitting a quiz correction. For example, if you score a 6\/10 on a quiz, you may upgrade your score to an 8\/10 with a quiz correction.       Exams     Midterm Exam  There is one Midterm Exam in this class, administered on Wednesday, April 30. There will be no calculator allowed on this exam, as all of your work will need to be shown by hand.   Practice Midterm: One week before the Midterm, I will release a Practice Midterm. Working on it together with your group will be one of the Workbook Activities, and you can make sure that you are prepared for the exam and support your classmates by preparing together.   Midterm Corrections: After your graded exam is returned, you may redo the exam in its entirety and submit it as a Midterm Correction. Your final grade will be an average of your two submissions, meaning you can recover up to half of your missed points by submitting a Midterm correction. For example, if you score a 74% on the exam, you may upgrade your score to an 87% with a midterm correction.    Final Exam  There is one Final Exam in this class, administered on Wednesday, June 11. There will be limited calculator use allowed on this exam. Unfortunately, since the Final will be during our last class meeting, there is no opportunity for a Final Correction.   Practice Final: One week before the Final, I will release a Practice Final. Working on it together with your group will be one of the Workbook Activities, and you can make sure that you are prepared for the exam and support your classmates by preparing together.       Late Work and Make-Up Policy  Mathematics is a subject that builds on itself, so it is crucial that you stay on top of your work at the pace of the class. However, I understand that life does not always line up with college schedules and deadlines. If you need more time to complete a homework assignment, or if you need to miss class, please contact me (see ) to work out an extension, or another way to make up work.     "
+},
+{
+  "id": "grades-2-2",
+  "level": "2",
+  "url": "grades.html#grades-2-2",
+  "type": "Table",
+  "number": "6.1",
+  "title": "Grades",
+  "body": " Grades    Assignment  Number  Weight    Homework Sets  7  20%    Workbook Activities  18  20%    Quizzes  7  10%    Midterm Exam  1  20%    Final Exam  1  30%    "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "7",
+  "title": "Policies",
+  "body": " Policies   Visit the PCC Policies page for information on:   Academic Integrity Policy    Accessibility Statement and Resources    Drop\/Withdraw Deadlines    Grading Policy    Internet Etiquette    PCC Payment Policy    PCC Registration Policy    Sanctuary College    Student Rights and Responsibilities    Title IX\/Non-Descrimination       Student Rights and Responsibilities   Students are required to complete this course in accordance with the Student Rights and Responsibilities Handbook. The Handbook establishes students' freedoms and protections as well as the expectations of appropriate behavior and ethical academic work. The Handbook includes items such as the Policy on Student Rights, and the Student Code of Conduct Policy and Procedures.    Academic Integrity  The handbook contains the Code of Student Conduct and the Academic Integrity Policy. Cheating includes any attempt to defraud, deceive, or mislead the instructor in arriving at an honest grade assessment, and may include copying answers from other students or using unauthorized notes during tests. Plagiarism is a particular form of cheating that involves presenting as one's own the ideas or work of another and may include using other people's ideas without proper attribution and submitting another person's work as one's own. Dishonest activities such as cheating on exams and submitting or copying work donw by others will result in disciplinary actions including but not limited to receiving a failing grade. For further information, review the institution's Academic Integrity Policy.     Artificial Intelligence Statement  The use of generative AI and Language Learning Models in this course such as, but not limited to ChatGPT, should be used only if it does not otherwise violate the Academic Integrity Policy of the College. Such violations include turning in work that is not your own.  In this class specifically, while generative AI and language learning models can be excellent tools for summarizing and presenting information, leaning on such tools when you are yourself trying to learn something, even in ways that seem to be academically honest, can instill a dependency that is really challenging to let go of later. I highly recommend avoiding the use of tools like ChatGPT, even as a study assistant! Please ask me for help instead.     Internet Etiquette   Click here for more information about Internet Etiquette (or Netiquette).     Sanctuary College Statement  PCC is a sanctuary college. For more information and resources, see the resources for undocumented students page.     Inclement Weather Statement  Our course is typically unaffected by college campus and facility closures or delayed class start times. Our regular due dates [and Zoom class meeting times (if a remote class)] apply. However, if PCC must close all operations, our regular due dates will be adjusted accordingly. Please check your email for my instructions and continue to check Brightspace during closures.    Flexibility Statement  The instructor reserves the right to modify course content and\/or substitute assignments and learning activities in response to institutional, weather, or class situations.   "
+},
+{
+  "id": "subsubsec-academic-integrity-2",
+  "level": "2",
+  "url": "policies.html#subsubsec-academic-integrity-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Cheating Plagiarism "
+},
+{
+  "id": "accessibility",
+  "level": "1",
+  "url": "accessibility.html",
+  "type": "Section",
+  "number": "8",
+  "title": "Accessibility",
+  "body": " Accessibility   Statement  This course uses digital courseware, software, or texts that may cause barriers for those using assistive, or accessibility-related technologies. If you encounter barriers, please contact the Access Tech Team at access-tech-group@pcc.edu or by calling 971-722-TECH (971-722-8324).    Accomodations  PCC is committed to supporting all students. If you plan to use academic accommodations for this course, please contact your instructor as soon as possible to discuss your needs. Accommodations are not retroactive; they begin when the instructor receives the “Approved Academic Accommodations” letter from you (submitted in person for courses on campus; via email for Online Learning courses). To request academic accommodations for a disability, please contact an Accessible Education and Disability Resources counselor on any PCC campus. Office locations, phone numbers, and additional information may be located on the Accessible Education and Disability Resources website.   "
+},
+{
+  "id": "calendar",
+  "level": "1",
+  "url": "calendar.html",
+  "type": "Section",
+  "number": "9",
+  "title": "Calendar",
+  "body": " Calendar   Course Calendar  Note that the specific dates of material and assignments in this calendar are subject to change as needed during the term.        Week  Monday  Wednesday  Assignments    3\/31-4\/6  Whole Number Arithmetic  Signed Numbers   Intro Form  (see )  Prob. Set 1 (due 4\/7)  Activity 1,2     4\/7-4\/13  Prime Numbers and Exponents  Factoring   Prob. Set 2 (due 4\/14)  Activity 3,4     4\/14-4\/20  Fractions  Fraction Multiplication   Prob. Set 3 (due 4\/21)  Activity 5,6     4\/21-4\/27  Fraction Simplification  Fraction Addition  Activity 7,8    4\/28-5\/4  Catch Up and Review  Midterm Exam   Activity 9  Self-Assessment 1     5\/5-5\/11  Decimals  Decimal Addition   Prob. Set 4 (due 5\/12)  Activity 10,11     5\/12-5\/18  Decimal Multiplication  Decimal Division   Prob. Set 5 (due 5\/21)  Activity 12,13     5\/19-5\/25   No Class  (School Holiday)   Ratios\/Proportions   Prob. Set 6 (due 5\/26)  Activity 14     5\/26-6\/1  Rates and Unit Costs  Percentages   Prob. Set 7 (due 6\/2)  Activity 15,16     6\/2-6\/8  Probability  Catch Up   Activity 17,18     6\/9-6\/15   No Class  (Finals Week)   Final Exam  Self-Assessment 2      Important Dates  See PCC's Academic Calendar for more information.      Add\/Drop Deadline   Tuesday, April 8  Last day to late register for the class  Last day to drop for a refund.     Midterm Exam   Wednesday, April 30  In Class. See .     College Closure   Monday, May 26  No Class     Withdraw Deadline   Saturday, June 7  Last day to withdraw (no refund, W on transcript)  Last day to change grade option.     Final Exam   Wednesday, June 11  2 PM, approximately 2 hours. See .     Grades Available  Tuesday, June 17       "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
